4fdf4bcfb87bb85a1fbc7391d7aa2659172f01ac
Михаил Лугин <329898@mail.ru>
2019-09-28 11:57:44 +0300

    goto CS

---
+++ NM115/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace NM115
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.IsShowPointValues = false;
+            this.zedGraphControl1.Location = new System.Drawing.Point(0, 0);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.PointValueFormat = "G";
+            this.zedGraphControl1.Size = new System.Drawing.Size(440, 414);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ NM115/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace NM115
+{
+    public partial class Form1 : Form
+    {
+
+        GraphPane graphPane;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            graphPane = zedGraphControl1.GraphPane;
+            DrawSineCurve();
+        }
+
+        private void DrawSineCurve()
+        {
+            PointPairList _pointPairList = new PointPairList();
+
+            for (int _angle = 0; _angle <= 360; _angle += 10)
+            {
+                double _x = _angle;
+                double _y = Math.Log(Math.PI * _x / 180.0);
+                PointPair _pointPair = new PointPair(_x, _y);
+
+                _pointPairList.Add(_pointPair);
+            }
+
+            LineItem _lineItem = graphPane.AddCurve("Sine Curve", _pointPairList, Color.Purple, SymbolType.None);
+
+            zedGraphControl1.AxisChange();
+        }
+
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
